d61d5a2d18107534dd1431cd4c9e0e6787ec7e74
GodNona <datanhpham181208@gmail.com>
2026-01-16 13:43:48 +0700

    fix currency mock
    use script có sẵn trong yandex plugin

--- Assets/PluginYourGames/Modules/Payments/Example/Scripts/ReceivingPurchaseExample.cs
+++ Assets/PluginYourGames/Modules/Payments/Example/Scripts/ReceivingPurchaseExample.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -29,7 +30,7 @@ namespace YG.Example
         {
             successPurchased?.Invoke();
 
-            textExample.text = "Success purchase - " + id;
+            //textExample.text = "Success purchase - " + id;
 
             // Ваш код для обработки покупки. Например:
 
@@ -42,13 +43,30 @@ namespace YG.Example
             //    YG2.SetState(coinsKey, coins + 250);
             //else if (id == "1500")
             //    YG2.SetState(coinsKey, coins + 1500);
+
+            int coin = id switch
+            {
+                "coin_100" => 100,
+                "coin_200" => 200,
+                "coin_500" => 500,
+                "coin_800" => 800,
+                "coin_1000" => 1000,
+                _ => 0
+            };
+            if (coin <= 0)
+            {
+                Debug.LogWarning("Unknown id: " + id);
+                return;
+            }
+            SaveManager.Instance.AddCoin(coin);
+            YG2.SaveProgress();
         }
 
         private void FailedPurchased(string id)
         {
             failedPurchased?.Invoke();
 
-            textExample.text = "Failed purchase - " + id;
+            //textExample.text = "Failed purchase - " + id;
         }
     }
 }
